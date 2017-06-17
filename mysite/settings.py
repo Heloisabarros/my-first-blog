e97b649d6e707a3a285b707e323c755a66a1bff4
heloisa <heloisabns@gmail.com>
2017-06-17 13:36:33 -0300

    My Django Girls app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h+i$6ssg6)-ussii@-kh&1s8%8*@px_*!&oqzex3s+ua31b56b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'heloisabns.pythonanywhere.com']
 
 
 # Application definition
